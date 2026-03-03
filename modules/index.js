7287d2a88c437321d3361092882fc1accdce4ab0
0x00webs <178827097+0x00webs@users.noreply.github.com>
2026-03-03 19:47:56 +0000

    feat: create index module to export minification utilities

---
+++ modules/index.js
@@ -0,0 +1,6 @@
+export * from './logger.js';
+export * from './minifyCss.js';
+export * from './minifyHtml.js';
+export * from './minifyImages.js';
+export * from './minifyJs.js';
+export * from './minifySvg.js';
